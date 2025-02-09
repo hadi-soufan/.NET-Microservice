a4a62b55f039682f0e89b8b9259e94dabdb4ce90
hadi-soufan <hady.a.soufan@gmail.com>
2025-02-09 18:08:19 +0200

    RECORD: added auction records

---
+++ src/AuctionService/Models/Records/Auctions/UpdateAuctionRecord.cs
@@ -0,0 +1,12 @@
+﻿namespace AuctionService.Models.Records.Auctions
+{
+    public sealed record UpdateAuctionRecord
+    (
+        Guid Id,
+        string Make,
+        string Model,
+        int Year,
+        string Color,
+        int Mileage
+    );
+}
