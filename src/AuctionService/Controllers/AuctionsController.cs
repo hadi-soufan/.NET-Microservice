1704f9a11a273d1a4b4f3d6ddbb96131d4665b39
hadi-soufan <hady.a.soufan@gmail.com>
2025-02-09 18:05:54 +0200

    CONTROLLER: added auction controller

---
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -0,0 +1,102 @@
+﻿using AuctionService.Models;
+using AuctionService.Models.Entities.Auctions;
+using AuctionService.Models.Records.Auctions;
+using MapsterMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Controllers
+{
+    [ApiController]
+    [Route("api/auctions")]
+    public class AuctionsController : ControllerBase
+    {
+        private readonly AuctionDbContext _context;
+        private readonly IMapper _mapper;
+
+        public AuctionsController(
+            AuctionDbContext context, 
+            IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<AuctionRecord>> CreateAsync(CreateAuctionRecord createAuctionRecord)
+        {
+            var auction = _mapper.Map<Auction>(createAuctionRecord);
+            auction.Seller = "test";
+
+            _context.Auctions.Add(auction);
+            var result = await _context.SaveChangesAsync() > 0;
+
+            if (!result) return BadRequest("Could not save changes to the DB.");
+
+            return CreatedAtAction(nameof(RetrieveById), new { id = auction.Id }, _mapper.Map<AuctionRecord>(auction));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<AuctionRecord>>> Retrieve()
+        {
+            var auctions = await _context.Auctions
+                .Include(a => a.Item)
+                .OrderBy(a => a.Item.Make)
+                .ToListAsync();
+
+            return _mapper.Map<List<AuctionRecord>>(auctions);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AuctionRecord>> RetrieveById(Guid id)
+        {
+            var auction = await _context.Auctions
+                .Include(a => a.Item)
+                .FirstOrDefaultAsync(a => a.Id == id);
+
+            if (auction is null)
+                return NotFound();
+
+            return _mapper.Map<AuctionRecord>(auction);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<AuctionRecord>> UpdateAsync(Guid id, UpdateAuctionRecord updateAuctionRecord)
+        {
+            var auction = await _context.Auctions
+                .Include(a => a.Item)
+                .FirstOrDefaultAsync(a => a.Id == id);
+
+            if (auction is null)  return NotFound();
+            
+
+            auction.Item.Make = updateAuctionRecord.Make ?? auction.Item.Make;
+            auction.Item.Model = updateAuctionRecord.Model ?? auction.Item.Model;
+            auction.Item.Year = updateAuctionRecord.Year != 0 ? updateAuctionRecord.Year : auction.Item.Year;
+            auction.Item.Color = updateAuctionRecord.Color ?? auction.Item.Color;
+            auction.Item.Mileage = updateAuctionRecord.Mileage != 0 ? updateAuctionRecord.Mileage : auction.Item.Mileage;
+
+
+            var result = await _context.SaveChangesAsync() > 0;
+            if (!result) return BadRequest("Could not save changes to the DB.");
+
+            return _mapper.Map<AuctionRecord>(auction);
+        }
+
+        [HttpPatch("{id}")]
+        public async Task<ActionResult<AuctionRecord>> UpdateStatusAsync(Guid id)
+        {
+            var auction = await _context.Auctions
+                .Include(a => a.Item)
+                .FirstOrDefaultAsync(a => a.Id == id);
+            if (auction is null) return NotFound();
+
+            auction.IsDeleted = true;
+            var result = await _context.SaveChangesAsync() > 0;
+            if (!result) return BadRequest("Could not save changes to the DB.");
+            return _mapper.Map<AuctionRecord>(auction);
+        }
+
+
+    }
+}
