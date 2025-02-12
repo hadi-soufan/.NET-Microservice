a83c3f2d59dce20fff088159a9f0b9d335e5197e
hadi-soufan <hady.a.soufan@gmail.com>
2025-02-12 18:32:00 +0200

    CONTRACT: created the required contracts

---
+++ src/Contracts/AuctionUpdated.cs
@@ -0,0 +1,12 @@
+﻿namespace Contracts
+{
+    public sealed record class AuctionUpdated
+    {
+        public string Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+        public int Mileag { get; set; }
+    }
+}
